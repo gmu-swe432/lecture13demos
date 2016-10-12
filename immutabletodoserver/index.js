8f8e6a4601b8f5a46b869f692369205964a1cf64
Jonathan Bell <jbell@cs.columbia.edu>
2016-10-12 18:03:01 -0400

    Initial commit

---
+++ immutabletodoserver/index.js
@@ -0,0 +1,29 @@
+var firebase = require("firebase");
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+
+firebase.initializeApp({
+    serviceAccount: "privkey.json",
+    databaseURL: "https://swe432lecture12.firebaseio.com"
+});
+var fireRef = firebase.database().ref('todos');
+
+var port = process.env.port || 3000;
+
+app.post('/newTodo', function (req, res) {
+    console.log("New req");
+    console.log("Client wants to create todo: '" + req.body.todoText + "'");
+    fireRef.push({"text": req.body.todoText}, function () {
+        res.send("OK!");
+    });
+});
+app.use(express.static('public'));
+
+app.listen(port, function () {
+    console.log('Example app listening on port ' + port);
+});
